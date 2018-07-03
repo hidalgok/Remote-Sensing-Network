200c29e534bbb8c883fa7edfee699ac114f7ecc6
hidalgok <kenchristopherhidalgo@gmail.com>
2018-07-03 11:06:12 -0600

    Nova and Plantower Sensors

---
+++ Plantower/Plantower.ino
@@ -0,0 +1,30 @@
+//Taken from here: https://github.com/fu-hsi/PMS
+
+#include <PMS.h>
+#include <SoftwareSerial.h>
+
+PMS pms(Serial1);
+PMS::DATA data;
+
+void setup()
+{
+  Serial.begin(9600);   // GPIO1, GPIO3 (TX/RX pin on ESP-12E Development Board)
+  Serial1.begin(9600);  // GPIO2 (D4 pin on ESP-12E Development Board)
+}
+
+void loop()
+{
+  if (pms.read(data))
+  {
+    Serial.print("PM 1.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_1_0);
+
+    Serial.print("PM 2.5 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_2_5);
+
+    Serial.print("PM 10.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_10_0);
+
+    Serial.println();
+  }
+}
